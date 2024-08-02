2918d9bab0cea130a10839f635a6b264f23bec3b
Dhruv Patel <dhruvpatel242231@gmail.com>
2024-08-02 21:48:22 +0530

    number with new line

--- Stringcal.js
+++ Stringcal.js
@@ -1,4 +1,12 @@
 function add(numbers) {
+  if (numbers.startsWith("//")) {
+    const delimiter = numbers[2];
+    numbers = numbers.slice(4);
+    const nums = numbers.split(delimiter).map(Number);
+
+    return nums.reduce((sum, num) => sum + num, 0);
+  }
+
   if (numbers === "") return 0; // emty string
   if (!isNaN(numbers)) return parseInt(numbers, 10);
 
--- stringcal.test.js
+++ stringcal.test.js
@@ -14,3 +14,10 @@ test("sum of the number returns the Sum", () => {
 test("number with new line", () => {
   expect(add("1,2\n,3")).toBe(6); // new line convert this type  1+2+3 =6;
 });
+
+test("custom delimiter returns sum of numbers", () => {
+  expect(add("//;\n1;2")).toBe(3); // custom delimiter and sum
+});
+test("custom delimiter returns sum of numbers", () => {
+  expect(add("1,-2,3")).toBe(-2); // custom delimiter and sum
+});
