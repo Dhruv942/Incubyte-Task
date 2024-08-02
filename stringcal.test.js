647118106a1d2d31b06c3b8502161d49e503e7f1
Dhruv Patel <dhruvpatel242231@gmail.com>
2024-08-02 18:49:39 +0530

    number with new line

--- stringcal.test.js
+++ stringcal.test.js
@@ -1,16 +1,16 @@
 const { add } = require("./Stringcal");
 
 test("empty string returns 0", () => {
-  expect(add("")).toBe(0);
+  expect(add("")).toBe(0); //empty string return 0
 });
 
 test("single number returns the number", () => {
-  expect(add("1")).toBe(1);
+  expect(add("2")).toBe(2); // single number output
 });
 test("sum of the number returns the Sum", () => {
-  expect(add("1,2")).toBe(3);
+  expect(add("1,2")).toBe(3); // comma and sum
 });
 
 test("number with new line", () => {
-  expect(add("1,2\n,3")).toBe(3);
+  expect(add("1,2\n,3")).toBe(6); // new line convert this type  1+2+3 =6;
 });
