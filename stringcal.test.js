9ed86ab1dfd0adae10ebf50f61d4a51d03f5b91b
Dhruv Patel <dhruvpatel242231@gmail.com>
2024-08-03 09:51:16 +0530

    comment console to check

--- stringcal.test.js
+++ stringcal.test.js
@@ -18,3 +18,24 @@ test("number with new line", () => {
 test("custom delimiter returns sum of numbers", () => {
   expect(add("//;\n1;2")).toBe(3); // custom delimiter and sum
 });
+
+/*
+//const { add } = require("./Stringcal");
+
+// Test for custom delimiters
+//console.log("Test: empty string");
+//console.log(add(""));
+
+console.log("Test: single number");
+console.log(add("1"));
+
+// Test for multiple numbers with commas
+console.log("Test: multiple numbers with commas");
+console.log(add("1,2")); // Expected output: 3
+
+// Test for new lines
+console.log("Test: numbers separated by new lines");
+console.log(add("1\n2,3,4")); // Expected output: 6
+
+console.log("Test: custom delimiter");
+console.log(add("//;\n1;2")); // Expected output: 3 */
